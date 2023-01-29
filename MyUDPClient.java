4ec0ff95ae8c53b163476385bd7aef9e18544188
Cedric Zheng <veryhappy13579@gmail.com>
2023-01-29 23:37:40 +0000

    rename vars and add seqnum to both request and response packet

--- MyUDPClient.java
+++ MyUDPClient.java
@@ -6,46 +6,63 @@ public class MyUDPClient {
 
     private final static int PORT = 7707;
     private static final String HOSTNAME = "localhost";
-    private static final byte[] ipAddr = new byte[] { 20, 106, 101, (byte) 156 };
+    private static int numPackets = 0;
+    private static boolean[] ackSeqnum;
+    // private static final byte[] ipAddr = new byte[] { 20, 106, 101, (byte) 156 };
+    private final static int bufferSize = 20;
 
     public static void main(String[] args) {
 
         try (DatagramSocket socket = new DatagramSocket(0)) {
             socket.setSoTimeout(1000);
-            // InetAddress host = InetAddress.getByName(HOSTNAME);
-            InetAddress host = InetAddress.getByAddress(ipAddr);
-            byte[] requestData = "get test.txt".getBytes();
-            DatagramPacket request = new DatagramPacket(requestData, requestData.length, host, PORT);
-            DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
-            socket.send(request);
+            InetAddress host = InetAddress.getByName(HOSTNAME);
+            // InetAddress host = InetAddress.getByAddress(ipAddr);
+            byte[] seqnumBytes = new byte[4];
+            byte[] requestData = new byte[bufferSize];
+            seqnumBytes = ByteBuffer.allocate(4).putInt(0).array();
+            System.arraycopy(seqnumBytes, 0, requestData, 0, 4);
+            byte[] messageBytes = "get test.txt".getBytes();
+            System.arraycopy(messageBytes, 0, requestData, 4, messageBytes.length);
+            DatagramPacket outPkt = new DatagramPacket(requestData, requestData.length, host, PORT);
+            DatagramPacket inPkt = new DatagramPacket(new byte[bufferSize], bufferSize);
+            socket.send(outPkt);
             try {
                 // process the packet...
-                socket.receive(response);
-                String result = new String(response.getData(), 0, response.getLength(), "US-ASCII");
+                socket.receive(inPkt);
+                String result = new String(inPkt.getData(), 0, inPkt.getLength(), "US-ASCII");
                 System.out.println(result);
                 String[] responseValues = result.split(" ");
-                int numPackets = Integer.parseInt(responseValues[0]);
+                numPackets = Integer.parseInt(responseValues[0]);
                 int windowSize = Integer.parseInt(responseValues[1]);
                 System.out.println("numPackets: " + numPackets + " windowSize: " + windowSize);
+                ackSeqnum = new boolean[numPackets + 1];
                 for (int i = 0; i < numPackets; i++) {
-                    requestData = new byte[1024];
+                    requestData = new byte[bufferSize];
                     requestData = ByteBuffer.allocate(4).putInt(i).array();
-                    request = new DatagramPacket(requestData, requestData.length, host, PORT);
-                    socket.send(request);
+                    outPkt = new DatagramPacket(requestData, requestData.length, host, PORT);
+                    socket.send(outPkt);
                 }
                 while (numPackets > 0) {
-                    socket.receive(response);
-                    byte[] seqnumBytes = new byte[4];
-                    System.arraycopy(response.getData(), 0, seqnumBytes, 0, 4);
-                    result = new String(response.getData(), 4, response.getLength() - 4, "US-ASCII");
-                    System.out.println(ByteBuffer.wrap(seqnumBytes).getInt());
-                    System.out.println(result);
+                    socket.receive(inPkt);
+                    seqnumBytes = new byte[4];
+                    System.arraycopy(inPkt.getData(), 0, seqnumBytes, 0, 4);
+                    int seqnum = ByteBuffer.wrap(seqnumBytes).getInt();
+                    ackSeqnum[seqnum] = true;
+                    result = new String(inPkt.getData(), 4, inPkt.getLength() - 4, "US-ASCII");
                     numPackets--;
+                    System.out.println(
+                            "No. " + seqnum + "   " + numPackets + " packets left");
+                    System.out.println(result);
                 }
 
             } catch (SocketTimeoutException ex) {
                 socket.close();
-                System.err.println("No connection within 10 seconds");
+                System.err.println("No connection within 1 seconds");
+                for (int i = 1; i <= numPackets; i++) {
+                    if (!ackSeqnum[i]) {
+                        System.out.println("packet No. " + i + " missing");
+                    }
+                }
 
             }
 
--- MyUDPServer.java
+++ MyUDPServer.java
@@ -1,6 +1,5 @@
 import java.nio.ByteBuffer;
 import java.net.*;
-import java.util.Date;
 import java.util.logging.*;
 import java.io.*;
 import java.util.*;
@@ -10,6 +9,8 @@ public class MyUDPServer {
     private final static int PORT = 7707;
     private final static Logger audit = Logger.getLogger("requests");
     private final static Logger errors = Logger.getLogger("errors");
+    private final static int bufferSize = 20;
+    private static int numPackets = 0;
 
     public static void main(String[] args) {
 
@@ -17,44 +18,43 @@ public class MyUDPServer {
             Map<Integer, byte[]> chunks = new HashMap<>();
             while (true) {
                 try {
-
-                    DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
-                    socket.receive(request);
-                    int seqnum = ByteBuffer.wrap(request.getData()).getInt();
-                    String requestString = new String(request.getData(), 0, request.getLength());
+                    DatagramPacket inPkt = new DatagramPacket(new byte[bufferSize], bufferSize);
+                    socket.receive(inPkt);
+                    byte[] seqnumBytes = new byte[4];
+                    System.arraycopy(inPkt.getData(), 0, seqnumBytes, 0, 4);
+                    int seqnum = ByteBuffer.wrap(seqnumBytes).getInt();
+                    String requestString = new String(inPkt.getData(), 4, inPkt.getLength() - 4);
                     audit.info(requestString);
-
-                    if (requestString.equals("get test.txt")) {
+                    if (requestString.contains("get test.txt")) {
                         FileInputStream fis = new FileInputStream("Content/test.txt");
                         int windowSize = 10;
-                        int bufferSize = 1024;
-                        int numPackets = (int) Math.ceil((double) fis.getChannel().size() / bufferSize);
+                        numPackets = (int) Math.ceil((double) fis.getChannel().size() / (bufferSize - 4));
                         byte[] data = (numPackets + " " + windowSize).getBytes();
 
-                        // System.out.println("Begin to send file ... ");
-
+                        audit.info("Begin to send file ... ");
                         // Here we break file into chunks
-                        int chunkNumber = 0;
+                        int chunkNumber = 1;
                         byte[] chunk = new byte[bufferSize];
-                        int bytesRead;
-                        while ((bytesRead = fis.read(chunk, 4, 1020)) > 0) {
+                        // int bytesRead;
+                        while (fis.read(chunk, 4, bufferSize - 4) > 0) {
                             byte[] chunkNumberArray = ByteBuffer.allocate(4).putInt(chunkNumber).array();
                             System.arraycopy(chunkNumberArray, 0, chunk, 0, 4);
                             chunks.put(chunkNumber, chunk);
                             chunkNumber++;
                             chunk = new byte[bufferSize];
                         }
-                        DatagramPacket response = new DatagramPacket(data, data.length, request.getAddress(),
-                                request.getPort());
-                        socket.send(response);
-                        audit.info(numPackets + " " + windowSize + " " + request.getAddress());
-                    } else if (seqnum >= 0 && seqnum < 10) {
-                        audit.info("seqnum: " + seqnum);
-                        DatagramPacket response = new DatagramPacket(chunks.get(seqnum),
+                        fis.close();
+                        DatagramPacket outPkt = new DatagramPacket(data, data.length, inPkt.getAddress(),
+                                inPkt.getPort());
+                        socket.send(outPkt);
+                        audit.info(numPackets + " " + windowSize + " " + inPkt.getAddress());
+                    } else if (seqnum > 0 && seqnum <= numPackets) {
+                        audit.info("request seqnum: " + seqnum);
+                        DatagramPacket outPkt = new DatagramPacket(chunks.get(seqnum),
                                 chunks.get(seqnum).length,
-                                request.getAddress(),
-                                request.getPort());
-                        socket.send(response);
+                                inPkt.getAddress(),
+                                inPkt.getPort());
+                        socket.send(outPkt);
 
                     }
                 } catch (IOException | RuntimeException ex) {
