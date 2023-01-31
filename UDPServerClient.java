c7d2cb61a364bafdf8e8a927cebf73d6a9045807
Cedric Zheng <veryhappy13579@gmail.com>
2023-01-31 05:55:06 +0000

    use exit to reset the mode

--- UDPServerClient.java
+++ UDPServerClient.java
@@ -7,48 +7,42 @@ public class UDPServerClient {
    public static void main(String[] args) throws Exception {
       Scanner scanner = new Scanner(System.in);
       String mode = "";
-      while (!mode.equals("exit")) {
+      DatagramSocket socket = new DatagramSocket(7707);
+      while (mode != "exit") {
          System.out.println("Enter mode (client/server/exit):");
          mode = scanner.nextLine();
          if (mode.equals("client")) {
-            DatagramSocket socket = new DatagramSocket();
-            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] ipAddr = new byte[] { 20, 106, 101, (byte) 156 };
+            InetAddress IPAddress = InetAddress.getByAddress(ipAddr);
             System.out.println("Starting UDP client");
             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
             while (true) {
                System.out.println("Enter a sentence to send to the server:");
                String sentence = inFromUser.readLine();
-               if (sentence.equals("server")) {
-                  mode = "server";
-                  break;
-               }
-               if (sentence.equals("exit")) {
-                  mode = "exit";
-                  break;
-               }
+
                byte[] sendData = sentence.getBytes();
                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7707);
                socket.send(sendPacket);
+               if (sentence.equals("exit")) {
+                  break;
+               }
                byte[] receiveData = new byte[1024];
                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                socket.receive(receivePacket);
                String modifiedSentence = new String(receivePacket.getData());
                System.out.println("FROM SERVER:" + modifiedSentence);
+
             }
          } else if (mode.equals("server")) {
-            DatagramSocket socket = new DatagramSocket(7707);
             System.out.println("Starting UDP server on port 7707");
             while (true) {
                byte[] receiveData = new byte[1024];
                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                socket.receive(receivePacket);
-               String sentence = new String(receivePacket.getData());
-               if (sentence.equals("client")) {
-                  mode = "client";
-                  break;
-               }
+               String sentence = new String(receivePacket.getData()).trim();
+               System.out.println("FROM CLIENT:" + sentence);
+
                if (sentence.equals("exit")) {
-                  mode = "exit";
                   break;
                }
                InetAddress IPAddress = receivePacket.getAddress();
