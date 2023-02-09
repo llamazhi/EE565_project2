6782ae424ad3c45d4e4f89e79692400832a78d58
Cedric Zheng <veryhappy13579@gmail.com>
2023-02-08 21:55:26 -0800

    add maps to store client info

--- UDPServer.java
+++ UDPServer.java
@@ -11,19 +11,20 @@ public class UDPServer extends Thread {
     private int port;
     private static int numChunks;
     private final static int bufferSize = 8192;
-    // TODO: map<filename, map<seqnum, chunks>>
-    private Map<Integer, byte[]> fileChunks;
+    private HashMap<SocketAddress, String> clientInfos;
+    private HashMap<String, ArrayList<Long>> chunkSendTimestamps;
+    private HashMap<String, Integer> pathToBitRate;
+    private HashMap<String, HashMap<Integer, byte[]>> fileChunks;
     private static int bitSent = 0;
     private static long startTime;
-
     private final static int MAX_WINDOW_SIZE = 100;
-    // private static int windowStart = 1;
-    // private static int windowEnd = MAX_WINDOW_SIZE;
 
     public UDPServer(int port) {
         this.port = port;
         this.fileChunks = new HashMap<>();
-
+        this.chunkSendTimestamps = new HashMap<>();
+        this.clientInfos = new HashMap<>();
+        this.pathToBitRate = new HashMap<>();
     }
 
     public static void intToByteArray(int value, byte[] buffer) {
@@ -40,16 +41,6 @@ public class UDPServer extends Thread {
                 (bytes[3] & 0xff);
     }
 
-    // TODO: use thread? channel? handle multiple client?
-    // TODO: limit sending rate at server side using the config in add peer
-    // TODO: use the config in peer/add?rate=xxx to limit the sending rate
-    // TODO: client specify the sending rate when sending the request
-
-    // time: 0 timer start
-    // long bitSent;
-    // for every outPkt, bitSent += outPkt.length * 8;
-    // if bitSent > rate: sleep until time = 1
-    // time = 1, bitSent = 0;
     @Override
     public void run() {
         try (DatagramSocket socket = new DatagramSocket(this.port)) {
@@ -72,15 +63,13 @@ public class UDPServer extends Thread {
 
         if (seqNum == 0 && !requestString.isEmpty()) {
             // request for a file
-            audit.info("Client request for file " + requestString);
             String[] requestValues = requestString.split(" ");
-            // TODO: requestString will also have sending rate
             String path = requestValues[0];
+            audit.info("Client request for file " + path);
             bitRate = Integer.parseInt(requestValues[1]);
-            VodServer.setBitRate(bitRate); // unit in bit/sec
-            startTime = System.currentTimeMillis();
+            this.pathToBitRate.put(path, bitRate); // unit in bit/sec
+            UDPServer.startTime = System.currentTimeMillis();
 
-            // TODO: if reveive new file request, add an entry to the fileChunks map
             File requestFile = new File(path);
             if (!requestFile.exists()) {
                 byte[] data = ("FileNotExistsError").getBytes(Charset.forName("US-ASCII"));
@@ -88,6 +77,9 @@ public class UDPServer extends Thread {
                 socket.send(outPkt);
                 return;
             }
+
+            audit.info("Client socket address: " + inPkt.getSocketAddress());
+            this.clientInfos.put(inPkt.getSocketAddress(), path);
 
             long fileSize = requestFile.length();
             long lastModified = requestFile.lastModified();
@@ -99,18 +91,23 @@ public class UDPServer extends Thread {
 
             audit.info("Begin to send file ... ");
 
-            // Read all the file into chunks
-            int chunkIndex = 1;
-            byte[] chunk = new byte[bufferSize];
-            FileInputStream fis = new FileInputStream(requestFile);
-            while (fis.read(chunk, 4, bufferSize - 4) > 0) {
-                intToByteArray(chunkIndex, chunk);
-                this.fileChunks.put(chunkIndex, chunk);
-                chunkIndex++;
-                chunk = new byte[bufferSize];
-            }
-            fis.close();
+            if (!this.fileChunks.containsKey(path)) {
+                audit.info("Read new file into memory" + path);
 
+                this.fileChunks.put(path, new HashMap<>());
+                // Read all the file into chunks
+                int chunkIndex = 1;
+                byte[] chunk = new byte[bufferSize];
+                FileInputStream fis = new FileInputStream(requestFile);
+                while (fis.read(chunk, 4, bufferSize - 4) > 0) {
+                    intToByteArray(chunkIndex, chunk);
+                    this.fileChunks.get(path).put(chunkIndex, chunk);
+                    chunkIndex++;
+                    chunk = new byte[bufferSize];
+                }
+                fis.close();
+
+            }
             // send response packet
             DatagramPacket outPkt = new DatagramPacket(data, data.length, inPkt.getAddress(), inPkt.getPort());
             socket.send(outPkt);
@@ -118,36 +115,37 @@ public class UDPServer extends Thread {
 
         } else if (seqNum > 0 && seqNum <= numChunks) {
             // request for specific chunk of data
-            // audit.info("Client request chunk packet at seqNum: " + seqNum);
-
-            // check if seqNum is within window
-            // release the buffer if the leftmost packet has been received by client
-            // TODO: map<filename, map<seqnum, chunks>>
-            // TODO: sleep to limit the sending rate
-
-            DatagramPacket outPkt = new DatagramPacket(this.fileChunks.get(seqNum),
-                    this.fileChunks.get(seqNum).length,
+            audit.info("Client socket address: " + inPkt.getSocketAddress());
+            String path = this.clientInfos.get(inPkt.getSocketAddress());
+            audit.info("Client request for: " + path);
+            byte[] sendingChunk = this.fileChunks.get(path).get(seqNum);
+            DatagramPacket outPkt = new DatagramPacket(
+                    sendingChunk,
+                    sendingChunk.length,
                     inPkt.getAddress(),
                     inPkt.getPort());
 
             bitSent += outPkt.getLength() * 8;
-            // System.out.println("bitSent: " + bitSent);
-            bitRate = (VodServer.getBitRate()); // unit in bit/sec
-            // System.out.println("bitRate: " + bitRate);
-            long elapsedTime = System.currentTimeMillis() - startTime;
-            // System.out.println("elapsedTime: " + elapsedTime);
-            if (bitRate != 0 && bitSent > bitRate && elapsedTime <= 1000) {
+            bitRate = this.pathToBitRate.get(path); // unit in kbps
+            long elapsedTime = System.currentTimeMillis() - UDPServer.startTime;
+
+            socket.send(outPkt);
+
+            if (bitRate != 0 && bitSent > bitRate * 1000 && elapsedTime <= 1000) {
                 try {
                     System.out.println("elapsed time: " + elapsedTime);
                     System.out.println("Need sleep for " + (1000 - elapsedTime) + " ms");
                     Thread.sleep(1000 - elapsedTime);
                     bitSent = 0;
-                    startTime = System.currentTimeMillis(); // update startTime
+                    UDPServer.startTime = System.currentTimeMillis(); // update startTime
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            socket.send(outPkt);
+            if (!this.chunkSendTimestamps.containsKey(path)) {
+                this.chunkSendTimestamps.put(path, new ArrayList<>());
+            }
+            this.chunkSendTimestamps.get(path).add(System.currentTimeMillis());
         }
     }
 }
